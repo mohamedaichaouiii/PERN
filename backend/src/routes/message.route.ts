b707616521fa99d4beedc1b64849f98ab51186c9
Mohamed <mohamed.aichaoui.tic@gmail.com>
2024-08-26 15:02:54 +0100

    controllers & routes for messages

--- backend/src/routes/message.route.ts
+++ backend/src/routes/message.route.ts
@@ -1,14 +1,10 @@
 import express from "express";
-
+import protectRoute from "../middleware/protectRoute.js";
+import { getMessages, getUsersForSidebar, sendMessage } from "../controllers/message.controller.js";
 const router = express.Router();
 
-router.get("/conversation", (req, res) => {
-    res.send("Conversation fetched successfully");
-});
-
-
-
-
-
+router.get("/conversations", protectRoute, getUsersForSidebar);
+router.get("/:id", protectRoute, getMessages);
+router.post("/send/:id", protectRoute, sendMessage);
 
 export default router;
