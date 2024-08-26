6822f79a8355ef87c1294dbbf3090d05ac1a7533
Mohamed <mohamed.aichaoui.tic@gmail.com>
2024-08-26 18:36:39 +0100

    AuthContext.tsx added

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { AuthContextProvider } from "./context/AuthContext.tsx";
+//import SocketContextProvider from "./context/SocketContext.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <BrowserRouter>
-    <App />
-    </BrowserRouter>
-    
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  // <React.StrictMode>
+  <BrowserRouter>
+    <AuthContextProvider>
+      <App />
+    </AuthContextProvider>
+  </BrowserRouter>
+  // </React.StrictMode>
+);
