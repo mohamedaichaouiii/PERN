98e8ebd92eb1271ff1c08fabd21f11b6e02148a3
Mohamed <mohamed.aichaoui.tic@gmail.com>
2024-08-27 07:24:34 +0100

    Messages

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,18 +1,17 @@
 import express from "express";
-import authRoutes from "./routes/auth.route.js"
-import messageRoutes from "./routes/message.route.js"
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
 dotenv.config();
-const app =express();
+const PORT = process.env.PORT || 5001;
+const app = express();
 app.use(cookieParser()); // for parsing cookies
 app.use(express.json()); // for parsing application/json
-app.use("/api/auth",authRoutes)
-app.use("/api/messages",messageRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
-
-app.listen(5000,()=>{
-    console.log("Server is runing on port 5000....");
+app.listen(PORT, () => {
+  console.log(`Server is runing on port ${PORT} ...`);
 });
-
--- frontend/src/components/hooks/useGetMessages.ts
+++ frontend/src/components/hooks/useGetMessages.ts
@@ -1,17 +1,17 @@
 import { useEffect, useState } from "react";
-import useConversation from "../zustand/useConversation";
+import useConversation from "../../zustand/useConversation";
 import toast from "react-hot-toast";
 
 const useGetMessages = () => {
 	const [loading, setLoading] = useState(false);
-	const { messages, setMessages, selectedConversation } = useConversation();
+	const { messages, setMessages, selectedConversation } = useConversation(); //from the library zustand predefined
 
 	useEffect(() => {
 		const getMessages = async () => {
 			if (!selectedConversation) return;
 			setLoading(true);
 			setMessages([]);
-			try {
+			try { //send request to the endpoint
 				const res = await fetch(`/api/messages/${selectedConversation.id}`);
 				const data = await res.json();
 				if (!res.ok) throw new Error(data.error || "An error occurred");
